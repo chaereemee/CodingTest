1ca6e9035ab533ab57af8cad15dfbb6a0c961adf
CHAERIM LEE <97179589+chaereemee@users.noreply.github.com>
2023-11-19 07:30:36 +0900

    [D3] Title: 구구단 걷기, Time: 474 ms, Memory: 53,320 KB -BaekjoonHub

---
+++ SWEA/D3/16800. 구구단 걷기/구구단 걷기.py
@@ -0,0 +1,14 @@
+import math
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    num = int(input())
+    
+    arr = []
+    for i in range(1, int(math.sqrt(num))+1):
+        if num % i == 0:
+            arr.append((i, num//i))
+    for i, (x, y) in enumerate(arr):
+        arr[i] = (x - 1) + (y - 1)
+    print("#{} {}".format(test_case, min(arr)))
