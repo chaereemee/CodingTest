517d2a4bd3f87de06318235a9a4062e10fe29e96
이채림 <97179589+chaereemee@users.noreply.github.com>
2024-10-31 20:41:34 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int days = ((V-B) / (A-B)) + ((V-B) % (A-B) == 0 ? 0 : 1);
+        System.out.println(days);
+    }
+}
