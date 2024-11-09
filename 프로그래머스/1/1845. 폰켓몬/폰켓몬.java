5fe18474d578734c7a9d01caa6dde1831f77ad78
이채림 <97179589+chaereemee@users.noreply.github.com>
2024-11-09 17:18:45 +0900

    [level 1] Title: 폰켓몬, Time: 5.91 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        int count = nums.length / 2;
         HashSet<Integer> set = new HashSet<>();
         
-        for(int i : nums) {
-            set.add(i);
+        for(int num : nums) {
+            set.add(num);
         }
         
-        if(set.size() < nums.length / 2)
+        if(set.size() < count)
             return set.size();
         else
-            return nums.length / 2;
+            return count;
     }
 }
