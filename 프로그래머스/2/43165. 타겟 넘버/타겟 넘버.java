e15da3a8f0a8cb81b43cc1c2bc515c76b29187ba
이채림 <97179589+chaereemee@users.noreply.github.com>
2024-11-12 12:25:26 +0900

    [level 2] Title: 타겟 넘버, Time: 6.79 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,13 +1,20 @@
 class Solution {
-    public int solution(int[] numbers, int target) {
-        return dfs(numbers, target, 0, 0);
+    static int answer;
+    
+    void DFS(int[] numbers, int target, int index, int sum) {
+        if(index == numbers.length) {
+            if(sum == target) {
+                answer++;
+            }
+            return;
+        }
+        
+        DFS(numbers, target, index+1, sum+numbers[index]);
+        DFS(numbers, target, index+1, sum-numbers[index]);
     }
     
-    private int dfs(int[] numbers, int target, int index, int sum) {
-        if(index == numbers.length)
-            return sum == target ? 1 : 0;
-        
-        return dfs(numbers, target, index+1, sum + numbers[index]) +
-            dfs(numbers, target, index+1, sum - numbers[index]);
+    public int solution(int[] numbers, int target) {
+        DFS(numbers, target, 0, 0);
+        return answer;
     }
 }
