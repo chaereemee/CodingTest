044e3c5c93db5247e31e23b3dbd3cd734364f00d
이채림 <97179589+chaereemee@users.noreply.github.com>
2024-11-26 21:17:24 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 48.70 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,9 @@
 class Solution {
     public String solution(int n) {
-        char[] patten = {'수', '박'};
         String str = "";
         
         for(int i=0; i<n; i++) {
-            if(i % 2 == 0) {
-                str += String.valueOf(patten[0]);
-            } else {
-                str += String.valueOf(patten[1]);
-            }
+            str += (i%2==0) ? "수" : "박";
         }
         
         return str;
