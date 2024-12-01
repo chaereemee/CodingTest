5777afad6557a9724f860b5033f6468cd809395a
이채림 <97179589+chaereemee@users.noreply.github.com>
2024-12-01 21:08:06 +0900

    [level 1] Title: 폰켓몬, Time: 5.16 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 5.91 ms
+메모리: 89.5 MB, 시간: 5.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 09일 17:18:42
+2024년 12월 01일 21:08:00
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int count = nums.length / 2;
-        HashSet<Integer> set = new HashSet<>();
+        int halfNums = nums.length / 2;
         
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int num : nums) {
-            set.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        int kind = map.size();
         
-        if(set.size() < count)
-            return set.size();
-        else
-            return count;
+        int result = kind > halfNums ? halfNums : kind;
+        return result;
     }
 }
