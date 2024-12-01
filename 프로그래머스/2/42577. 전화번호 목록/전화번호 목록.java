7336f43be53f9c061eff4d81c0f9bd7117838373
이채림 <97179589+chaereemee@users.noreply.github.com>
2024-12-01 21:39:26 +0900

    [level 2] Title: 전화번호 목록, Time: 356.47 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,9 +4,10 @@ class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         
-        for(int i=1; i<phone_book.length; i++) {
-            if(phone_book[i].startsWith(phone_book[i-1]))
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {
                 return false;
+            }
         }
         return true;
     }
